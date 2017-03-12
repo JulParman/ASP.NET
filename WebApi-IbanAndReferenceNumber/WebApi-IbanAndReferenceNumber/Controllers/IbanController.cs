1aa5fb1c3f87f89f08ef12e6243a03847cfe020f
JulParman <julian.parman@gmail.com>
2017-03-12 22:12:15 +0200

    WebApi added

---
+++ WebApi-IbanAndReferenceNumber/WebApi-IbanAndReferenceNumber/Controllers/IbanController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi_IbanAndReferenceNumber.Controllers
+{
+    public class IbanController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Check(string ibanNumber)
+        {
+            var iban = new WebApi_IbanAndReferenceNumber.Models.Iban();
+            var result = iban.CheckIbanValidation(ibanNumber);
+            return View("Index", result);
+        }
+    }
+}
---
+++ WebApi-IbanAndReferenceNumber/WebApi-IbanAndReferenceNumber/Models/Iban.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_IbanAndReferenceNumber.Models
+{
+    public class Iban
+    {
+    public string IbanNumber { get; set; }
+        private string[] letters = new string[] {"A","B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q","R","S","T","U","V","W","X","Y","Z"};
+        private int[] numbers = new int[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35 };
+
+        public string CheckIbanValidation(string ibanNumber)
+        {            
+            ulong divider = 97;
+            ulong reminder = 0;
+            string fourCharsToEnd = ibanNumber.Substring(0, 4);
+            ibanNumber = ibanNumber.Remove(0, 4);
+            ibanNumber = ibanNumber.Insert(ibanNumber.Count(), fourCharsToEnd);
+            int index = ibanNumber.IndexOf("F");
+            string letterConverted = Letters(ibanNumber.Substring(index,2));
+            ibanNumber = ibanNumber.Replace("FI", letterConverted);
+            ulong number = ulong.Parse(ibanNumber);
+            reminder = (number) % divider;           
+
+            if(reminder == 1)
+            {
+                return "Iban is correct!";
+            }
+            else
+            {
+                return "Iban is incorrect!";
+            }
+        }
+        private string Letters(string s)
+        {
+            string lettersToNum = "";
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (s.Contains(letters[i]))
+                {
+                    lettersToNum = s.Replace(letters[i], numbers[i].ToString());
+                    s = lettersToNum;
+                }
+            }
+            return lettersToNum;
+        }
+
+        
+
+
+    }
+}
